b383e4bd08e56ffc6addcc18073396e4fa16ca16
dimchoSCR <dimchokarpachev@gmail.com>
2018-12-31 13:57:51 +0200

    Model BellServiceException

---
+++ src/main/java/spring/BellServiceException.java
@@ -0,0 +1,14 @@
+package spring;
+
+import javax.xml.ws.WebServiceException;
+
+public class BellServiceException extends WebServiceException {
+
+    public BellServiceException(String message) {
+        super(message);
+    }
+
+    public BellServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
