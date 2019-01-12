0d55483fdfd012f7e821c08481a87204dc0ce284
dimchoSCR <dimchokarpachev@gmail.com>
2019-01-12 12:06:35 +0200

    Made setRingtone consumable by android client

--- src/main/java/smartbell/restapi/MelodiesController.java
+++ src/main/java/smartbell/restapi/MelodiesController.java
@@ -24,7 +24,7 @@ public class MelodiesController {
     }
 
     @PutMapping("/update/ringtone")
-    public String setRingtone(@RequestParam("name") String melodyName) {
+    public String setRingtone(@RequestBody String melodyName) {
         return melodyManager.setAsRingtone(melodyName);
     }
 }
