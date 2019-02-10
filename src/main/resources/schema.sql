7d80f446b67eba63d3b335c69bbe934727ed98c1
dimchoSCR <dimchokarpachev@gmail.com>
2019-02-10 17:49:19 +0200

    Edit primary key constraint

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS main;
 
 CREATE TABLE IF NOT EXISTS main.RingLog (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  id BIGINT AUTO_INCREMENT,
   ringtone_name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(1),
+  constraint main.RingLog
+    primary key (id)
 );
 
