37d0ca1a43b230b54b6bf4ae82602e54de746900
dimchoSCR <dimchokarpachev@gmail.com>
2019-03-03 19:48:48 +0200

    Switch button pin to PIN_0(17 BCM)

--- src/main/java/smartbell/backend/model/GPIO.java
+++ src/main/java/smartbell/backend/model/GPIO.java
@@ -5,11 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum GPIO implements PinNumberSystem {
-    PIN_1("1"), PIN_2("2"), PIN_27("27");
+    PIN_0("0"), PIN_1("1"), PIN_2("2"), PIN_27("27");
 
     private static final Map<String, String> wPiToBcmMapping;
     static {
         Map<String, String> backingMap = new HashMap<>();
+        backingMap.put("0", "17");
         backingMap.put("1", "18");
         backingMap.put("2", "27");
         backingMap.put("27", "16");
